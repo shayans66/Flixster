99825b65c44ad4a6d1a01939f3cecfe87fd26033
shayans66 <35939705+shayans66@users.noreply.github.com>
2021-05-31 15:55:23 -0700

    cleaner without window onlad

--- script.js
+++ script.js
@@ -3,56 +3,62 @@ const API_KEY = "2d578ff54b28db88d467ae4065cd2bdb";
 
 let list_of_movies = [];
 
-window.onload = function () {
-  const result = fetch(
-    "https://api.themoviedb.org/3/movie/now_playing?api_key=" +
-      API_KEY +
-      "&language=en-US&page=1"
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
 
-      // for each movie
-      for (movie of data["results"]) {
-        // create movie elem container div with inner container class
-        let movieElem = document.createElement("div");
-        movieElem.classList.add("movie-item");
+const result = fetch(
+"https://api.themoviedb.org/3/movie/now_playing?api_key=" +
+  API_KEY +
+  "&language=en-US&page=1"
+)
+  .then((res) => res.json())
+  .then((data) => {
+    // console.log(data);
 
-        // create title
-        let movieElemTitle = document.createElement("p");
-        movieElemTitle.style.textAlign = "center";
-        movieElemTitle.innerHTML = movie["original_title"];
-        movieElemTitle.style.margin = 0;
+    // for each movie
+    for (movie of data["results"]) {
+      // create movie elem container div with inner container class
+      let movieElem = document.createElement("div");
+      movieElem.classList.add("movie-item");
 
-        // create rating
-        let movieElemRating = document.createElement("p");
-        movieElemRating.style.textAlign = "center";
-        movieElemRating.innerHTML = "⭐" + movie["vote_average"];
-        movieElemRating.style.margin = 0;
+      // create title
+      let movieElemTitle = document.createElement("p");
+      movieElemTitle.style.textAlign = "center";
+      movieElemTitle.innerHTML = movie["original_title"];
+      movieElemTitle.style.margin = 0;
 
-        // create movie element to be added to dom
-        let movieElemImg = document.createElement("img");
-        movieElemImg.src = img_prefix + movie["backdrop_path"];
-        movieElemImg.alt = movie["original_title"];
+      // create rating
+      let movieElemRating = document.createElement("p");
+      movieElemRating.style.textAlign = "center";
+      movieElemRating.innerHTML = "⭐" + movie["vote_average"];
+      movieElemRating.style.margin = 0;
 
-        // append poster, title, rating, break
-        movieElem.appendChild(movieElemImg);
-        movieElem.appendChild(movieElemTitle);
-        movieElem.appendChild(movieElemRating);
-        movieElem.appendChild(document.createElement("br"));
+      // create movie element to be added to dom
+      let movieElemImg = document.createElement("img");
+      movieElemImg.src = img_prefix + movie["backdrop_path"];
+      movieElemImg.alt = movie["original_title"];
 
-        document.getElementById("movies").appendChild(movieElem);
-        // list_of_movies.push(movieElem)
+      // append poster, title, rating, break
+      movieElem.appendChild(movieElemImg);
+      movieElem.appendChild(movieElemTitle);
+      movieElem.appendChild(movieElemRating);
+      movieElem.appendChild(document.createElement("br"));
 
-        // console.log(movieElem);
-      }
-    })
-    .catch((error) => console.log(error));
-};
+      document.getElementById("movies").appendChild(movieElem);
+      // list_of_movies.push(movieElem)
 
-// for(el of list_of_movies){
-//   console.log(el)
+      
+    }
+  })
+.catch((error) => console.log(error));
+
+
+// console.log(list_of_movies);
+
+// async function listMovies() {
+//   try {
+//     const response = await 
+//   } catch(err){
+//     console.log(err)
+//   }
 // }
-// console.log('h:')
-// console.log(list_of_movies)
+
+// listMovies()
