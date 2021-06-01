adc82300248fc308fb31848c12e0a44d6988cb43
shayans66 <35939705+shayans66@users.noreply.github.com>
2021-05-31 17:03:38 -0700

    starting pagination

--- script.js
+++ script.js
@@ -3,59 +3,14 @@ const API_KEY = "2d578ff54b28db88d467ae4065cd2bdb";
 
 let list_of_movies = [];
 
-
-// const result = fetch(
-// "https://api.themoviedb.org/3/movie/now_playing?api_key=" +
-//   API_KEY +
-//   "&language=en-US&page=1"
-// )
-//   .then((res) => res.json())
-//   .then((data) => {
-//     // console.log(data);
-
-//     // for each movie
-//     for (movie of data["results"]) {
-//       // create movie elem container div with inner container class
-//       let movieElem = document.createElement("div");
-//       movieElem.classList.add("movie-item");
-
-//       // create title
-//       let movieElemTitle = document.createElement("p");
-//       movieElemTitle.style.textAlign = "center";
-//       movieElemTitle.innerHTML = movie["original_title"];
-//       movieElemTitle.style.margin = 0;
-
-//       // create rating
-//       let movieElemRating = document.createElement("p");
-//       movieElemRating.style.textAlign = "center";
-//       movieElemRating.innerHTML = "⭐" + movie["vote_average"];
-//       movieElemRating.style.margin = 0;
-
-//       // create movie element to be added to dom
-//       let movieElemImg = document.createElement("img");
-//       movieElemImg.src = img_prefix + movie["backdrop_path"];
-//       movieElemImg.alt = movie["original_title"];
-
-//       // append poster, title, rating, break
-//       movieElem.appendChild(movieElemImg);
-//       movieElem.appendChild(movieElemTitle);
-//       movieElem.appendChild(movieElemRating);
-//       movieElem.appendChild(document.createElement("br"));
-
-//       document.getElementById("movies").appendChild(movieElem);
-//       // list_of_movies.push(movieElem)
-
-      
-//     }
-//   })
-// .catch((error) => console.log(error));
+let PAGE = 1;
 
 
-async function getMovies() {
+async function getMovies(PAGE) {
   const result = await fetch(
     "https://api.themoviedb.org/3/movie/now_playing?api_key=" +
       API_KEY +
-      "&language=en-US&page=1"
+      "&language=en-US&page=" + PAGE
     )
   data = await result.json();
   
@@ -80,7 +35,7 @@ async function getMovies() {
 
     // create movie element to be added to dom
     let movieElemImg = document.createElement("img");
-    movieElemImg.src = img_prefix + movie["backdrop_path"];
+    movieElemImg.src = img_prefix + movie["poster_path"];
     movieElemImg.alt = movie["original_title"];
 
     // append poster, title, rating, break
@@ -89,8 +44,8 @@ async function getMovies() {
     movieElem.appendChild(movieElemRating);
     movieElem.appendChild(document.createElement("br"));
 
-    // document.getElementById("movies").appendChild(movieElem);
-    list_of_movies.push(movieElem)
+    document.getElementById("movies").appendChild(movieElem);
+    // list_of_movies.push(movieElem)
 
     
   }
@@ -99,19 +54,19 @@ async function getMovies() {
 }
   
 
-async function listMovies() {
-  try {
-    const response = await getMovies()
+// async function listMovies() {
+//   try {
+//     const response = await getMovies()
 
-    for(el of list_of_movies){
-      document.getElementById("movies").appendChild(el);
-    }
+//     for(el of list_of_movies){
+//       document.getElementById("movies").appendChild(el);
+//     }
 
 
-  } 
-  catch(err){
-    console.log(err)
-  }
-}
+//   } 
+//   catch(err){
+//     console.log(err)
+//   }
+// }
 
-listMovies()
+getMovies(PAGE)
